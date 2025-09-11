cbb573a36bf3aa823fa232859384345db81c3c39
Maksym Ochenashko <maxochenashko@gmail.com>
2025-09-11 12:06:24 +0300

    Use _SC_HOST_NAME_MAX variable for a hostname buffer

--- sdk/common/native/src/main/scala/org/typelevel/otel4s/sdk/resource/HostDetectorPlatform.scala
+++ sdk/common/native/src/main/scala/org/typelevel/otel4s/sdk/resource/HostDetectorPlatform.scala
@@ -56,7 +56,7 @@ private[resource] trait HostDetectorPlatform { self: HostDetector.type =>
 
     private def detectHost: Option[String] =
       Zone.acquire { implicit z =>
-        val size = 256.toUSize
+        val size = _SC_HOST_NAME_MAX.toCSize
         val hostnameBuffer = alloc[CChar](size)
         if (gethostname(hostnameBuffer, size) == 0) {
           val hostname = fromCString(hostnameBuffer)
